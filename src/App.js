25d08eb0cf0ede73348759e4fd95af9d5b75327d
Sam Moss <sam.moss321@gmail.com>
2020-03-11 12:44:59 -0700

    pulls in react-redux

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Home.js";
 
 function App() {
   return (
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Home extends React.Component {
+  render() {
+    return <p>Home Screen</p>;
+  }
+}
+
+export default Home;
