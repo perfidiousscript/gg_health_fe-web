99945f54a677cd391a6e0e2573c7c0c9c10330db
Sam Moss <sam.moss321@gmail.com>
2020-03-12 09:08:11 -0700

    fixes typo in store/index

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
 
-const store = creatStore(rootReducer);
+const store = createStore(rootReducer);
 
 export default store;
