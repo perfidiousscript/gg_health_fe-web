21166b41d3b9a51f7764b4fc7e1c4342dcf0ffce
Sam Moss <sam.moss321@gmail.com>
2020-03-12 15:18:04 -0700

    adds some action type constants for future work

--- src/js/constants/action_types.js
+++ src/js/constants/action_types.js
@@ -1,1 +1,15 @@
+//User constants
+export const AUTHENTICATE_USER = "AUTHENTICATE_USER";
+export const CREATE_USER = "CREATE_USER";
+export const GET_USERS = "GET_USERS";
+export const UPDATE_USER = "UPDATE_USER";
+
+//Location Constants
 export const ADD_LOCATION = "ADD_LOCATION";
+export const UPDATE_LOCATION = "UPDATE_LOCATION";
+export const LOCATIONS_SEARCH = "LOCATIONS_SEARCH";
+
+//Practice Constants
+export const CREATE_PRACTICE = "CREATE_PRACTICE";
+export const UPDATE_PRACTICE = "UPSDATE_PRACTICE";
+export const GET_PRACTICE_LOCATIONS = "GET_PRACTICE_LOCATIONS";
