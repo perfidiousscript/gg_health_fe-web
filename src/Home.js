2a23f3a49dc1b8fc9b6f8f6732d50e3fd5f86da8
Sam Moss <sam.moss321@gmail.com>
2020-03-18 18:58:31 -0700

    adds Sign Up and Sign In links to home page

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
   render() {
-    return <p>Home Screen</p>;
+    return (
+      <div>
+        <p>Home Screen</p>
+        <Link to="/sign-up">Sign Up</Link>
+        <span> </span>
+        <Link to="/sign-in">Sign In</Link>
+      </div>
+    );
   }
 }
 
