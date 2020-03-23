778c47c108f486013f478d87f5a85b079a207c03
Sam Moss <sam.moss321@gmail.com>
2020-03-22 19:17:22 -0700

    adds notes to ManagerDash component

--- src/ManagerDash.js
+++ src/ManagerDash.js
@@ -2,9 +2,32 @@ import React from "react";
 import Store from "./js/store/index.js";
 import { connect } from "react-redux";
 
+// The purpose of this Component
+// is to display the Logged in user's
+// practice and locations, and allow them to update
+// that information, if needed.
+
+// In the future it will allow them to check their calender
+// Payments, client notes et c.
+
 class ManagerDash extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const { dispatch } = this.props;
+    // dispatch(fetchUserLocations());
+  }
   render() {
-    return <h3>Manager Dashboard!</h3>;
+    return (
+      <div>
+        <h3>Manager Dashboard!</h3>
+        <p>Practice Information</p>
+        <p>Your Locations</p>
+        <p>View/Manage Practitioners</p>
+      </div>
+    );
   }
 }
 
