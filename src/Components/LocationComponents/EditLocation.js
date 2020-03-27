ec5451c0fd835e4ca1106039db61358c70881ee9
Sam Moss <sam.moss321@gmail.com>
2020-03-27 16:49:09 -0700

    adds initial work to EditLocation component

--- src/Components/LocationComponents/EditLocation.js
+++ src/Components/LocationComponents/EditLocation.js
@@ -2,13 +2,81 @@ import React from "react";
 import Store from "../../js/store/index.js";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { Formik, Form, Field } from "formik";
 import { Container, Col, Row, Button } from "react-bootstrap";
 
 import PropTypes from "prop-types";
 
 class EditLocation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { location: this.props.location.state.location };
+  }
+
+  servicesRows() {
+    return (
+      <Row>
+        <Col>Services Here!</Col>
+      </Row>
+    );
+  }
+
   render() {
-    return <div>Edit Location!</div>;
+    const { dispatch, isFetching } = this.props;
+    const { location } = this.state.location;
+    console.log("location: ", location);
+    return (
+      <div>
+        <h4>Edit Location!</h4>
+        <Formik
+          initialValues={{
+            name: location.name,
+            address: location.address,
+            phone_number: location.phone_number
+          }}
+          onSubmit={values => {
+            // dispatch(editLocation(values));
+          }}
+        >
+          {({ isFetching, responseStatus }) => (
+            <Form>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <label for="name">Location Name</label>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <Field type="text" name="name" /> <br />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <label for="name">Address </label>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <Field type="text" name="address" /> <br />
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <label for="name">Phone Number</label>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={{ span: 2, offset: 4 }}>
+                  <Field type="text" name="phone_number" /> <br />
+                </Col>
+              </Row>
+              {this.servicesRows()}
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
   }
 }
 
