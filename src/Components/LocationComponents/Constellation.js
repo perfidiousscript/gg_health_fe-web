6c51c3816f31cd35b0165365d0e03f7b924a6ae7
Sam Moss <sam.moss321@gmail.com>
2020-04-03 15:37:12 -0700

    adds svg el, accesses locations from link

--- src/Components/LocationComponents/Constellation.js
+++ src/Components/LocationComponents/Constellation.js
@@ -4,10 +4,31 @@ import { connect } from "react-redux";
 import { Container, Col, Row, Button } from "react-bootstrap";
 
 class Constellation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { locations: this.props.location.state.locations };
+  }
+  drawShapes(locations) {}
   render() {
+    const { locations } = this.state;
+
     return (
       <div>
-        <h4>COnstellation Here!</h4>
+        <h4>Constellation Here!</h4>
+        <svg
+          className="line-container"
+          xmlns="http://www.w3.org/2000/svg"
+          width="600" //{this.props.width}
+          height="600" //{this.props.height}
+          style={{
+            backgroundColor: "indigo",
+            borderWidth: "thin",
+            borderColor: "yellow",
+            borderRadius: "50%"
+          }}
+        ></svg>
+        {this.drawShapes(locations)}
       </div>
     );
   }
