b27c57b1e168245157f0bb20791fa52bb5169c89
Sam Moss <sam.moss321@gmail.com>
2020-04-06 14:48:03 -0700

    places labels

--- src/js/d3/constellation_script.js
+++ src/js/d3/constellation_script.js
@@ -5,6 +5,16 @@ export default function drawConstellations(locations) {
     height = 500,
     radius = Math.min(width, height) / 2 - 30;
 
+  var xScale = d3
+    .scaleLinear()
+    .domain([-1, 1])
+    .range([-width / 2, width / 2]);
+
+  var yScale = d3
+    .scaleLinear()
+    .domain([-1, 1])
+    .range([-height / 2, height / 2]);
+
   var r = d3
     .scaleLinear()
     .domain([0, 2])
@@ -18,7 +28,26 @@ export default function drawConstellations(locations) {
     .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 1.2 + ")");
 
-  var modalityArray = Object.keys(locations);
+  let modalityArray = [];
+
+  function generateModalityArray(locations) {
+    var initialArray = Object.keys(locations);
+    var interval = 180 / (initialArray.length - 1);
+    initialArray.map((entry, index) => {
+      let capitalizedEntry = entry.charAt(0).toUpperCase() + entry.slice(1);
+      let degree = index * interval + 180;
+      modalityArray.push([capitalizedEntry, degree]);
+    });
+  }
+
+  function degreesToRadians(degrees) {
+    var pi = Math.PI;
+    return degrees * (pi / 180);
+  }
+
+  generateModalityArray(locations);
+
+  console.log("modalityArray: ", modalityArray);
 
   var circle = d3
     .select("g")
@@ -59,15 +88,15 @@ export default function drawConstellations(locations) {
 
   var textLabels = text
     .attr("x", function(d) {
-      return d.cx;
+      return xScale(0.6 * Math.cos(degreesToRadians(d[1])) - 0.1);
     })
     .attr("y", function(d) {
-      return d.cy;
+      return yScale(1.1 * Math.sin(degreesToRadians(d[1])));
     })
     .text(function(d) {
-      return `${d}`;
+      return `${d[0]}`;
     })
-    .attr("font-family", "sans-serif")
+    .attr("font-family", "serif")
     .attr("font-size", "20px")
-    .attr("fill", "dark-grey");
+    .attr("fill", "yellow");
 }
