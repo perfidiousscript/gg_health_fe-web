6137b9586411e7e33996cce27b98797b6ed281e3
Sam Moss <sam.moss321@gmail.com>
2020-04-06 13:49:47 -0700

    adds constellation background

--- src/js/d3/constellation_script.js
+++ src/js/d3/constellation_script.js
@@ -1,29 +1,73 @@
 import * as d3 from "d3";
 
 export default function drawConstellations(locations) {
-  var symbolGenerator = d3
-    .symbol()
-    .type(d3.symbolStar)
-    .size(80);
+  var width = 960,
+    height = 500,
+    radius = Math.min(width, height) / 2 - 30;
 
-  var points = [
-    [0, 80],
-    [100, 100],
-    [200, 30],
-    [300, 50],
-    [400, 40],
-    [500, 80]
-  ];
+  var r = d3
+    .scaleLinear()
+    .domain([0, 2])
+    .range([0, radius]);
 
-  var pathData = symbolGenerator();
+  var svg = d3
+    .select("d3Body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 1.2 + ")");
 
-  d3.select("g")
-    .selectAll("path")
-    .data(points)
+  var modalityArray = Object.keys(locations);
+
+  var circle = d3
+    .select("g")
+    .append("circle")
+    .attr("fill", "url(#svgGradient)")
+    .attr("r", 400);
+
+  var defs = svg.append("defs");
+
+  var gradient = defs
+    .append("linearGradient")
+    .attr("id", "svgGradient")
+    .attr("x1", "0%")
+    .attr("x2", "100%")
+    .attr("y1", "0%")
+    .attr("y2", "100%");
+
+  gradient
+    .append("stop")
+    .attr("class", "start")
+    .attr("offset", "0%")
+    .attr("stop-color", "purple")
+    .attr("stop-opacity", 1);
+
+  gradient
+    .append("stop")
+    .attr("class", "end")
+    .attr("offset", "100%")
+    .attr("stop-color", "blue")
+    .attr("stop-opacity", 1);
+
+  var text = d3
+    .select("g")
+    .selectAll("text")
+    .data(modalityArray)
     .enter()
-    .append("path")
-    .attr("transform", function(d) {
-      return "translate(" + d + ")";
+    .append("text");
+
+  var textLabels = text
+    .attr("x", function(d) {
+      return d.cx;
     })
-    .attr("d", pathData);
+    .attr("y", function(d) {
+      return d.cy;
+    })
+    .text(function(d) {
+      return `${d}`;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "20px")
+    .attr("fill", "dark-grey");
 }
