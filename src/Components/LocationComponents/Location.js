34ec5790485e0e1b0e8eb5961f1ac627315a7ba4
Sam Moss <sam.moss321@gmail.com>
2020-04-13 11:23:10 -0700

    fills in Location component

--- src/Components/LocationComponents/Location.js
+++ src/Components/LocationComponents/Location.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class Location extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { location: this.props.location.state.location };
+  }
   render() {
+    const { location } = this.state;
+
     return (
       <div>
-        <h4> Location Here!</h4>
+        <h4>{location.name}</h4>
+        <p> Primary Service: {location.services.primary_service}</p>
+        <p> Phone Number: {location.phone_number}</p>
       </div>
     );
   }
