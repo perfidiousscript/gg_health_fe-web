b572ab0e864bbf4828c776d6fdba2b94a34b3611
Sam Moss <sam.moss321@gmail.com>
2020-04-14 12:29:37 -0700

    some ground work for Calendar in Location component

--- src/Components/LocationComponents/Location.js
+++ src/Components/LocationComponents/Location.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./Location.css";
 
 class Location extends React.Component {
   constructor(props) {
@@ -6,6 +9,16 @@ class Location extends React.Component {
 
     this.state = { location: this.props.location.state.location };
   }
+
+  onClick = date => {
+    let parsedDate = Date(date);
+    let dateObject = {};
+    dateObject.year = date.getFullYear();
+    dateObject.month = date.getMonth();
+    dateObject.date = date.getDate();
+    console.log("dateObject: ", dateObject);
+  };
+
   render() {
     const { location } = this.state;
 
@@ -14,6 +27,12 @@ class Location extends React.Component {
         <h4>{location.name}</h4>
         <p> Primary Service: {location.services.primary_service}</p>
         <p> Phone Number: {location.phone_number}</p>
+        <Calendar
+          calendarType="US"
+          onClickDay={this.onClick}
+          value={this.state.date}
+          // tileContent={this.generateContent()}
+        />
       </div>
     );
   }
