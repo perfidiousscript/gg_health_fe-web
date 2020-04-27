6471d9e34b428af8e4775c2dd8529361b1e71ded
Sam Moss <sam.moss321@gmail.com>
2020-04-27 13:43:01 -0700

    connects Constellation to store, sets up logic to check whether locations are present, and adds conditional rendering

--- src/Components/LocationComponents/Constellation.js
+++ src/Components/LocationComponents/Constellation.js
@@ -1,27 +1,45 @@
 import React from "react";
-import * as d3 from "d3";
+import { connect } from "react-redux";
 import drawConstellations from "../../js/d3/constellation_script.js";
+import { fetchLocations } from "../../js/actions/location_actions.js";
 
 class Constellation extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { locations: this.props.location.state.locations };
-  }
-
   componentDidMount() {
-    const { locations } = this.state;
-    drawConstellations(locations, this);
+    const { dispatch, locations, location } = this.props;
+
+    let type;
+    let practiceId;
+    if (location.state) {
+      type = location.state.type;
+      practiceId = location.state.practice;
+    }
+
+    if (Object.keys(locations) == 0) {
+      dispatch(fetchLocations(type, practiceId));
+    }
   }
 
   render() {
-    return (
-      <div>
-        <h4>Constellation Here!</h4>
-        <div id="d3Body" style={{ display: "inline-block" }} />
-      </div>
-    );
+    const { isFetching, locations } = this.props;
+
+    if (isFetching & (Object.keys(locations) == 0)) {
+      return <p>"Loading..."</p>;
+    } else {
+      drawConstellations(locations, this);
+      return (
+        <div>
+          <h4>Constellation Here!</h4>
+          <div id="d3Body" style={{ display: "inline-block" }} />
+        </div>
+      );
+    }
   }
 }
 
-export default Constellation;
+function mapStateToProps(state) {
+  const { locations, isFetching } = state.locations;
+
+  return { locations };
+}
+
+export default connect(mapStateToProps)(Constellation);
