e4345c235c894281e8995cdafc6efac8d7214df3
Sam Moss <sam.moss321@gmail.com>
2020-04-28 14:28:06 -0700

    AppointmentModal form validation, EOD, WIP

--- src/Components/LocationComponents/AppointmentModal.js
+++ src/Components/LocationComponents/AppointmentModal.js
@@ -3,13 +3,23 @@ import Calendar from "react-calendar";
 import { connect } from "react-redux";
 import { Col, Row, Modal, Button } from "react-bootstrap";
 import { Formik, Form, Field } from "formik";
+import { addAppointment } from "../../js/actions/location_actions.js";
 
 class AppointmentModal extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dates: {}
+      dates: {},
+      startTimeHour: 9,
+      startTimeMinute: 15,
+      startTimeAmPm: "AM",
+      endTimeHour: 9,
+      endTimeMinute: 30,
+      endTimeAmPm: "AM",
+      service: "",
+      isValid: false,
+      error: { type: "", message: "" }
     };
   }
 
@@ -67,7 +77,8 @@ class AppointmentModal extends React.Component {
   };
 
   handleSelect = newDate => {
-    const { dates } = this.state;
+    const { dates, error } = this.state;
+
     let newDateObject = {};
     if (dates.date) {
       if (dates.date <= newDate) {
@@ -82,10 +93,12 @@ class AppointmentModal extends React.Component {
     }
 
     this.setState({ dates: newDateObject });
+    if (error.type === "date") {
+      this.setState({ error: { type: "", message: "" } });
+    }
   };
 
   serviceOptions = services => {
-    console.log("services: ", services);
     let servicesHtml = [];
 
     services.services_list.map(function(service, index) {
@@ -99,14 +112,112 @@ class AppointmentModal extends React.Component {
     return servicesHtml;
   };
 
+  //TO DO: Find a way to validate on new values, as this currently runs before state is updated and it uses the old values.
+  //Figure out if this needs to be done via redux.
+  validate = () => {
+    const {
+      dates,
+      startTimeHour,
+      startTimeMinute,
+      startTimeAmPm,
+      endTimeHour,
+      endTimeMinute,
+      endTimeAmPm,
+      service
+    } = this.state;
+
+    let startTime;
+    let endTime;
+
+    if (Object.keys(dates).length === 0) {
+      this.setState({
+        isValid: false,
+        error: { type: "date", message: "Please select at least one date!" }
+      });
+    } else if (dates.date) {
+      if (startTimeAmPm === "PM") {
+        startTimeHour += 12;
+      }
+      if (endTimeAmPm === "PM") {
+        endTimeHour += 12;
+      }
+
+      startTime = Date.UTC(
+        dates.date.getYear() + 1900,
+        dates.date.getMonth(),
+        dates.date.getDate(),
+        startTimeHour,
+        startTimeMinute
+      );
+
+      endTime = Date.UTC(
+        dates.date.getYear() + 1900,
+        dates.date.getMonth(),
+        dates.date.getDate(),
+        endTimeHour,
+        endTimeMinute
+      );
+
+      if (startTime < endTime) {
+        this.setState({
+          isValid: true,
+          error: {
+            type: "",
+            message: ""
+          }
+        });
+      } else {
+        this.setState({
+          isValid: false,
+          error: {
+            type: "endTime",
+            message: "Appointment Start Time must come before End Time!"
+          }
+        });
+      }
+    } else if (dates.start) {
+    }
+  };
+
+  changeAndValidate = e => {
+    this.onChange(e);
+    this.validate();
+  };
+
+  onChange = e => {
+    e.preventDefault();
+
+    let id = e.target.id;
+    let value = e.target.value;
+
+    this.setState({
+      [id]: value
+    });
+  };
+
+  onSubmit = () => {
+    console.log("state: ", this.state);
+  };
+
   render() {
-    const { handleClose, services } = this.props;
-    const { dates } = this.state;
+    const { handleClose, services, isFetching } = this.props;
+    const {
+      dates,
+      startTimeHour,
+      startTimeMinute,
+      startTimeAmPm,
+      endTimeHour,
+      endTimeMinute,
+      endTimeAmPm,
+      service,
+      isValid,
+      error
+    } = this.state;
 
     return (
       <>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment Modal!</Modal.Title>
+          <Modal.Title>Create Appointment</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {this.showDates(dates)}
@@ -115,63 +226,89 @@ class AppointmentModal extends React.Component {
             selectRange
             onClickDay={this.handleSelect}
           />
-          <Formik
-            onSubmit={values => {
-              // dispatch(addAppointment(values));
-            }}
-          >
-            {({ isFetching, responseStatus }) => (
-              <Form>
-                <Row>
-                  <Col md={{ span: 6 }}>
-                    <label name="startTime">Start Time: </label>
-                  </Col>
+          <Row>
+            <Col md={{ span: 6 }}>
+              <label name="startTime">Start Time: </label>
+            </Col>
 
-                  <Col md={{ span: 6 }}>
-                    <label name="endTime">End Time: </label>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={{ span: 6 }}>
-                    <select id="startTimeHour">{this.hourOptions()}</select>
-                    <select id="startTimeMinutes">
-                      {this.minuteOptions()}
-                    </select>
-                    <select id="startTimeamPm">
-                      <option value="am">AM</option>
-                      <option value="pm">PM</option>
-                    </select>
-                  </Col>
-                  <Col md={{ span: 6 }}>
-                    <select id="endTimeHour">{this.hourOptions()}</select>
-                    <select id="endTimeMinutes">{this.minuteOptions()}</select>
-                    <select id="endTimeamPm">
-                      <option value="am">AM</option>
-                      <option value="pm">PM</option>
-                    </select>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col md={{ span: 6 }}>
-                    <label name="services">Choose Service to offer: </label>
+            <Col md={{ span: 6 }}>
+              <label name="endTime">End Time: </label>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ span: 6 }}>
+              <select
+                id="startTimeHour"
+                value={startTimeHour}
+                onChange={this.onChange}
+              >
+                {this.hourOptions()}
+              </select>
+              <select
+                id="startTimeMinute"
+                value={startTimeMinute}
+                onChange={this.onChange}
+              >
+                {this.minuteOptions()}
+              </select>
+              <select
+                id="startTimeAmPm"
+                value={startTimeAmPm}
+                onChange={this.onChange}
+              >
+                <option value="am">AM</option>
+                <option value="pm">PM</option>
+              </select>
+            </Col>
+            <Col md={{ span: 6 }}>
+              <select
+                id="endTimeHour"
+                value={endTimeHour}
+                onChange={this.changeAndValidate}
+              >
+                {this.hourOptions()}
+              </select>
+              <select
+                id="endTimeMinute"
+                value={endTimeMinute}
+                onChange={this.changeAndValidate}
+              >
+                {this.minuteOptions()}
+              </select>
+              <select
+                id="endTimeAmPm"
+                value={endTimeAmPm}
+                onChange={this.changeAndValidate}
+              >
+                <option value="am">AM</option>
+                <option value="pm">PM</option>
+              </select>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ span: 6 }}>
+              <label name="service">Choose Service to offer: </label>
+              <select id="service" value={service} onChange={this.onChange}>
+                {this.serviceOptions(services)}
+              </select>
+            </Col>
+          </Row>
 
-                    <select id="serviceTypes">
-                      {this.serviceOptions(services)}
-                    </select>
-                  </Col>
-                </Row>
-              </Form>
-            )}
-          </Formik>
+          <Row>
+            <Col md={{ span: 6 }}>
+              <Button
+                type="submit"
+                disabled={isFetching || !isValid}
+                onClick={this.onSubmit}
+              >
+                Create Appointment
+              </Button>
+            </Col>
+            <Col md={{ span: 6 }}>
+              <p>{error.message}</p>
+            </Col>
+          </Row>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="primary" onClick={this.onSubmit}>
-            Create Appointment
-          </Button>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
       </>
     );
   }
