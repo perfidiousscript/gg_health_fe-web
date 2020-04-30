ff22d15f5d93a70f737634ddb2a34c95a93677e1
Sam Moss <sam.moss321@gmail.com>
2020-04-30 16:45:43 -0700

    applies redux-promise-middleware to store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "../reducers/root_reducer";
+import promise from "redux-promise-middleware";
 
-export default createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export default createStore(
+  rootReducer,
+  applyMiddleware(promise, thunkMiddleware)
+);
